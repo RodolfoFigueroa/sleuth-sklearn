aa50160e23fb936d18d02eb8167a2884c17ceb23
RodolfoFigueroa <4rodolfofigueroa2@gmail.com>
2023-05-31 10:10:57 -0600

    Update calibration script

--- sleuth_sklearn/calibrate.py
+++ sleuth_sklearn/calibrate.py
@@ -1,138 +1,65 @@
 import argparse
-import os
-
-import numpy as np
-import pandas as pd
-import sleuth_sklearn.utils as su
+import toml
 
 from pathlib import Path
-from sklearn.model_selection import GridSearchCV
 from sleuth_sklearn.estimator import SLEUTH
+from sleuth_sklearn.utils import open_dataset
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "filename",
-        help="Input file with all grids.",
-        type=str,
+        "path",
+        help="The path to the config file.",
+        type=str
     )
-    parser.add_argument(
-        "-s",
-        "--stage",
-        help="Initial stage. If greater than one, the previous stage files must exist.",
-        type=int,
-        choices=[1, 2, 3],
-        required=False,
-        default=1,
-    )
-    parser.add_argument(
-        "-d",
-        "--directory",
-        help="The directory where to store the generated stage files, or look for them if --stage is greater than 1.",
-        type=str,
-        required=False,
-        default="./",
-    )
-    parser.add_argument(
-        "-i",
-        "--iters",
-        help="Number of Monte Carlo iterations to run.",
-        type=int,
-        required=False,
-        default=10,
-    )
-    parser.add_argument(
-        "-r",
-        "--random-seed",
-        help="Seed to use for the PRNG.",
-        type=int,
-        required=False,
-        default=42,
-    )
-    parser.add_argument(
-        "-c",
-        "--critical-slope",
-        help="The critical slope coefficient.",
-        type=float,
-        required=False,
-        default=50,
-    )
-    parser.add_argument(
-        "-v",
-        "--verbose",
-        help="The level of verbosity to use for GridSearchCV.",
-        type=int,
-        required=False,
-        default=0,
-    )
-
+    
     args = parser.parse_args()
 
-    stage_dir = Path(args.directory)
-    ds = su.open_dataset(args.filename)
+    config_path = Path(args.path)
+    with open(config_path, "r") as f:
+        config = toml.load(f)
 
-    wanted_years = [year for year in ds.year.values if year >= 2000]
-    urban_grids = ds.sel(year=wanted_years)["urban"].values
+    ds = open_dataset(config["paths"]["data"])
 
-    param_grid_base = dict(
-        coef_diffusion=[1, 25, 50, 75, 100],
-        coef_breed=[1, 25, 50, 75, 100],
-        coef_spread=[1, 25, 50, 75, 100],
-        coef_slope=[1, 25, 50, 75, 100],
-        coef_road=[1, 25, 50, 75, 100],
+    if "random_seed" in config["montecarlo"]:
+        random_seed = config["montecarlo"]["random_seed"]
+    else:
+        random_seed = None
+    
+    model = SLEUTH(
+        n_iters=config["montecarlo"]["iterations"],
+        n_refinement_iters=config["refinement"]["iterations"],
+        n_refinement_splits=config["refinement"]["splits"],
+        n_refinement_winners=config["refinement"]["winners"],
+        coef_range_breed=config["coefficients"]["breed"],
+        coef_range_diffusion=config["coefficients"]["diffusion"],
+        coef_range_road=config["coefficients"]["road"],
+        coef_range_slope=config["coefficients"]["slope"],
+        coef_range_spread=config["coefficients"]["spread"],
+        grid_excluded=ds["excluded"].values,
+        grid_roads=ds["roads"].values,
+        grid_roads_dist=ds["dist"].values,
+        grid_roads_i=ds["road_i"].values,
+        grid_roads_j=ds["road_j"].values,
+        grid_slope=ds["slope"].values,
+        crit_slope=config["misc"]["critical_slope"],
+        random_state=random_seed,
+        n_jobs=config["multiprocessing"]["threads"]
     )
 
-    if args.stage == 1:
-        df = None
-        stages = [1, 2, 3]
-        param_grid = param_grid_base
+    if "start_year" in config["calibration"]:
+        start_year = config["calibration"]["start_year"]
+        
+        if start_year not in ds["year"].values:
+            raise Exception(f"Year {start_year} not in available observations.")
+            
+        wanted_years = [year for year in ds["year"].values if year >= start_year]
+        
+        X = ds["urban"].sel(year=wanted_years).values
+        y = wanted_years
     else:
-        path = stage_dir / f"./stage_{args.stage - 1}.csv"
-        if not os.path.exists(path):
-            raise FileNotFoundError("Stage file not found.")
-        df = pd.read_csv(path)
+        X = ds["urban"].values
+        y = ds["year"].values
 
-        if args.stage == 2:
-            stages = [2, 3]
-            param_grid = su.get_new_params(param_grid_base, df)
-
-    for stage in stages:
-        print(param_grid)
-
-        model = SLEUTH(
-            total_mc=args.iters,
-            grid_slope=ds["slope"].values,
-            grid_excluded=ds["excluded"].values,
-            grid_roads=ds["roads"].values,
-            grid_roads_i=ds["road_i"].values,
-            grid_roads_j=ds["road_j"].values,
-            grid_roads_dist=ds["dist"].values,
-            random_state=args.random_seed,
-            crit_slope=args.critical_slope,
-        )
-
-        gs = GridSearchCV(
-            estimator=model,
-            cv=[(slice(None), slice(None))],
-            param_grid=param_grid,
-            n_jobs=-1,
-            verbose=args.verbose,
-        )
-
-        res = gs.fit(urban_grids, wanted_years)
-        df = pd.DataFrame(res.cv_results_)
-        df.to_csv(stage_dir / f"./stage_{stage}.csv", index=False)
-
-        top = df.sort_values("rank_test_score").head(3)
-
-        new_param_grid = {}
-        for field in ["diffusion", "breed", "spread", "slope", "road"]:
-            col = top[f"param_coef_{field}"]
-            c_min = col.min()
-            c_max = col.max()
-
-            key = f"coef_{field}"
-            new_param_grid[key] = su.get_new_range(param_grid[key], c_min, c_max)
-
-        param_grid = new_param_grid
+    model.fit(X, y)
